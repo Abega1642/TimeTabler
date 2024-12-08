074a4fee2153e0a8b735da15378468498fabb549
Abega1642 <hei.abega.2@gmail.com>
2024-12-08 20:35:21 +0300

    feat(elements): implement 'Day'

---
+++ src/main/java/dev/razafindratelo/timeTabler/elements/Day.java
@@ -0,0 +1,26 @@
+package dev.razafindratelo.timeTabler.elements;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+public class Day {
+    private DayOfWeek dayOfWeek;
+    private LocalDate date;
+    private DayType dayType;
+
+
+    private enum DayType {
+        DAY_OFF, OPE,_DAY
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Day: [day=%s][date=%s][type=%s]", dayOfWeek, date, dayType);
+    }
+}
